025e903a804638994eeb4724b8ca5a06b08d89d8
lloop <alecleeeeee@gmail.com>
2025-02-06 18:11:35 +0800

    项目初始化

---
+++ src/main/java/com/lloop/Main.java
@@ -0,0 +1,11 @@
+package com.lloop;
+
+/**
+ * @Author lloop
+ * @Create 2025/2/6 18:11
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
