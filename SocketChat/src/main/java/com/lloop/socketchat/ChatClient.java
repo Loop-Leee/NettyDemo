916502661ed41a261035e532a4447fe0fe87623d
lloop <alecleeeeee@gmail.com>
2025-04-22 14:09:18 +0800

    客户端的发送和接收线程拆分成两个 Runnable 类

--- SocketChat/src/main/java/com/lloop/socketchat/ChatClient.java
+++ SocketChat/src/main/java/com/lloop/socketchat/ChatClient.java
@@ -6,24 +6,41 @@ import java.util.Scanner;
 
 public class ChatClient {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 8888);
+        try (Socket socket = new Socket("127.0.0.1", 8888);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              Scanner scanner = new Scanner(System.in)) {
 
             // 启动一个线程接收服务器消息
-            new Thread(() -> {
-                try {
-                    String msg;
-                    while ((msg = in.readLine()) != null) {
-                        System.out.println(msg);
-                    }
-                } catch (IOException e) {
-                    System.out.println("服务器连接断开");
-                }
-            }).start();
+            Receive receive = new Receive(socket);
+            new Thread(receive).start();
 
-            // 控制台输入用户名和消息
+            // 启动一个线程发送消息
+            Send send = new Send(socket);
+            Thread sendThread = new Thread(send);
+            sendThread.start();
+            sendThread.join();
+
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("连接服务器失败: " + e.getMessage());
+        }
+    }
+}
+
+class Send implements Runnable {
+    private Socket socket;
+
+    public Send(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             Scanner scanner = new Scanner(System.in);) {
+
             while (true) {
                 String input = scanner.nextLine();
                 if ("exit".equalsIgnoreCase(input)) {
@@ -33,6 +50,26 @@ public class ChatClient {
             }
         } catch (IOException e) {
             System.out.println("连接服务器失败: " + e.getMessage());
+        }
+    }
+}
+
+class Receive implements Runnable {
+    private Socket socket;
+
+    public Receive(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            String msg;
+            while ((msg = in.readLine()) != null) {
+                System.out.println(msg);
+            }
+        } catch (IOException e) {
+            System.out.println("服务器连接断开");
         }
     }
 }
